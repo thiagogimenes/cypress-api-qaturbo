dfbb406c9e039e629b2aca0983b5cc93c2c96099
thiagogimenes <thiagoaticus@gmail.com>
2026-03-08 23:09:11 -0300

    inicia projeto de automação de api

---
+++ cypress/e2e/buscar_dispositivo.cy.js
@@ -0,0 +1,35 @@
+describe('Buscar todos materiais', () => {
+
+    it('Deve buscar dispositivo com sucesso', () => {
+
+        cy.request({
+            method: "GET",
+            url: "https://api.restful-api.dev/objects/1"
+        }).then((resposta) => {
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.id).to.equal('1')
+            expect(resposta.body.name).to.equal('Google Pixel 6 Pro')
+        })
+    });
+
+    it('Deve deve cadastrar um dispositivo com sucesso', () => {
+
+        cy.request({
+            method: "POST",
+            url: "https://api.restful-api.dev/objects",
+            body: {
+                "name": "Meu Celular",
+                "data": {
+                    "year": 2019,
+                    "price": 1849.99,
+                    "CPU model": "Intel Core i9",
+                    "Hard disk size": "1 TB"
+                }
+            }
+        }).then((resposta) => {
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.name).to.equal('Meu Celular')
+            expect(resposta.body.id).not.empty
+        })
+    });
+});
